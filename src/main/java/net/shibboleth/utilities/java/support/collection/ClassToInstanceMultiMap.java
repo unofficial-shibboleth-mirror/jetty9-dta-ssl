493a3ed59d7fef44899a1d28210cfa28d04d4c73
Brent Putman <putmanb@georgetown.edu>
2013-08-16 20:05:55 +0000

    Import ClassToInstanceMultiMap, originally from metadata aggregator project.

---
+++ src/main/java/net/shibboleth/utilities/java/support/collection/ClassToInstanceMultiMap.java
@@ -0,0 +1,260 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.utilities.java.support.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * An append-only multimap where each entry associates a raw type (i.e. class) to instances of that class. In addition
+ * the map may also associate any supertype (i.e. all superclasses and interfaces implemented by the class) with the
+ * class.
+ * 
+ * Null values are not supported.
+ * 
+ * @param <B> a bound for the types of values in the map
+ */
+@NotThreadSafe
+public final class ClassToInstanceMultiMap<B> {
+
+    /** Whether supertypes should also be indexed. */
+    private final boolean indexSupertypes;
+
+    /** Map which backs this map. */
+    private final HashMap<Class<?>, List<B>> backingMap;
+
+    /** List of values that are indexed. */
+    private final List<B> values;
+
+    /** Constructor. Does not index supertypes. */
+    public ClassToInstanceMultiMap() {
+        this(false);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param isIndexingSupertypes indicates whether supertypes of a value should be indexed
+     */
+    public ClassToInstanceMultiMap(final boolean isIndexingSupertypes) {
+        backingMap = new HashMap<Class<?>, List<B>>();
+        values = new ArrayList<B>();
+        indexSupertypes = isIndexingSupertypes;
+    }
+
+    /** Removes all mappings from this map. */
+    public void clear() {
+        values.clear();
+        backingMap.clear();
+    }
+
+    /**
+     * Returns true if the map contains a mapping for the given key.
+     * 
+     * @param key key to check for in the map
+     * 
+     * @return true if the map contains a mapping for the specified key
+     */
+    public boolean containsKey(final Class<?> key) {
+        if (key == null) {
+            return false;
+        }
+
+        return backingMap.containsKey(key);
+    }
+
+    /**
+     * Returns true if the map contains a mapping to the given value.
+     * 
+     * @param value value to check for in this map
+     * 
+     * @return true if the map contains a mapping to the specified value
+     */
+    public boolean containsValue(final B value) {
+        if (value == null) {
+            return false;
+        }
+
+        return values.contains(value);
+    }
+
+    /**
+     * Gets the instances mapped to the given type or an empty list, immutable, list otherwise.
+     * 
+     * @param <T> type identifier
+     * @param type map key
+     * 
+     * @return instances mapped to the given type or an empty list, immutable, list otherwise
+     */
+    public <T> List<T> get(final Class<T> type) {
+        if (type == null) {
+            return Collections.emptyList();
+        }
+
+        final List<T> indexedValues = (List<T>) backingMap.get(type);
+        if (indexedValues == null) {
+            return Collections.emptyList();
+        }
+        return Collections.unmodifiableList(indexedValues);
+    }
+
+    /**
+     * Returns true if this map contains no entries, false otherwise.
+     * 
+     * @return true if this map contains no entries, false otherwise
+     */
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+
+    /**
+     * Gets the set of keys contained in this map. The set is backed by the map so changes made to the map are reflected
+     * in the set. However the set does not allow direct modification, any changes need to be done through this map.
+     * 
+     * @return set of keys contained in this map
+     */
+    public Set<Class<?>> keys() {
+        return Collections.unmodifiableSet(backingMap.keySet());
+    }
+
+    /**
+     * Adds a value to this map. If {@link #indexSupertypes} is false only the values class type is used as a key to the
+     * value. If {@link #indexSupertypes} is true, then the class types, all its supertypes, and all implemented
+     * interfaces are used as keys to the value.
+     * 
+     * Duplicate values, as determined by the values {@link Object#hashCode()} and {@link Object#equals(Object)}
+     * methods, are not stored. Only one instance of the value is ever stored in the map.
+     * 
+     * @param value value to be stored in the map
+     */
+    public void put(final B value) {
+        if (value == null) {
+            return;
+        }
+
+        if (!values.contains(value)) {
+            values.add(value);
+        }
+
+        final ArrayList<Class<?>> valueTypes = new ArrayList<Class<?>>();
+        valueTypes.add(value.getClass());
+
+        if (indexSupertypes) {
+            getSuperTypes(value.getClass(), valueTypes);
+        }
+
+        List<B> indexValues;
+        for (Class<?> valueType : valueTypes) {
+            indexValues = backingMap.get(valueType);
+
+            if (indexValues == null) {
+                indexValues = new ArrayList<B>();
+                backingMap.put(valueType, indexValues);
+            }
+
+            indexValues.add(value);
+        }
+    }
+
+    /**
+     * Adds all the values to the map.
+     * 
+     * @param newValues values to be added
+     * 
+     * @see ClassToInstanceMultiMap#put(Object)
+     */
+    public void putAll(final Iterable<? extends B> newValues) {
+        if (newValues == null) {
+            return;
+        }
+
+        for (B value : newValues) {
+            put(value);
+        }
+    }
+
+    /**
+     * Adds all the values to the map. This operations operates only on the given map's value collection. Therefore,
+     * regardless of the given map's policy on indexing by value supertypes, this map will index values based on its
+     * policy.
+     * 
+     * @param newValues values to be added
+     * 
+     * @see ClassToInstanceMultiMap#put(Object)
+     */
+    public void putAll(final ClassToInstanceMultiMap<? extends B> newValues) {
+        if (newValues == null) {
+            return;
+        }
+
+        for (B value : newValues.values()) {
+            put(value);
+        }
+    }
+
+    /**
+     * The collection of values currently present in the map. This collection is backed by the map so changeds to the
+     * map will be reflected in the collection. However the collection does not allow direct modification so any changes
+     * must be done through this map.
+     * 
+     * @return collection of values currently present in the map
+     */
+    public Collection<? extends B> values() {
+        return Collections.unmodifiableList(values);
+    }
+
+    /**
+     * Gets all of the superclasses and interfaces implemented by the given class.
+     * 
+     * @param clazz class for which supertypes will be determined
+     * @param accumulator collection to which supertypes are added as they are determined
+     */
+    private void getSuperTypes(final Class<?> clazz, final ArrayList<Class<?>> accumulator) {
+        final Class<?> superclass = clazz.getSuperclass();
+        if (superclass != null && superclass != Object.class) {
+            accumulator.add(superclass);
+            getSuperTypes(superclass, accumulator);
+        }
+
+        final Class<?>[] interfaces = clazz.getInterfaces();
+        if (interfaces.length > 0) {
+            for (Class<?> iface : interfaces) {
+                accumulator.add(iface);
+                getSuperTypes(iface, accumulator);
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    public int hashCode() {
+        // TODO
+        return super.hashCode();
+    }
+
+    /** {@inheritDoc} */
+    public boolean equals(Object obj) {
+        // TODO
+        return super.equals(obj);
+    }
+}
---
+++ src/test/java/net/shibboleth/utilities/java/support/collection/ClassToInstanceMultiMapTest.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.utilities.java.support.collection;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.Instant;
+import org.joda.time.ReadableDateTime;
+import org.joda.time.ReadableInstant;
+import org.joda.time.base.AbstractDateTime;
+import org.joda.time.base.AbstractInstant;
+import org.joda.time.base.BaseDateTime;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/** Unit test for {@link ClassToInstanceMultiMap}. */
+public class ClassToInstanceMultiMapTest {
+
+    @Test public void testClearIsEmpty() {
+        ClassToInstanceMultiMap<Object> map = new ClassToInstanceMultiMap<Object>();
+
+        map.clear();
+        Assert.assertTrue(map.isEmpty());
+
+        map.put(new Object());
+        Assert.assertFalse(map.isEmpty());
+
+        map.clear();
+        Assert.assertTrue(map.isEmpty());
+    }
+
+    @Test public void testKeysAndContainsKey() {
+        ClassToInstanceMultiMap<AbstractInstant> map = new ClassToInstanceMultiMap<AbstractInstant>();
+        populate(map);
+        Assert.assertEquals( map.keys().size(), 2);
+        Assert.assertFalse(map.containsKey(null));
+        Assert.assertFalse(map.containsKey(Chronology.class));
+        Assert.assertFalse(map.containsKey(AbstractInstant.class));
+        Assert.assertFalse(map.containsKey(AbstractDateTime.class));
+        Assert.assertFalse(map.containsKey(BaseDateTime.class));
+        Assert.assertTrue(map.containsKey(DateTime.class));
+        Assert.assertFalse(map.containsKey(Comparable.class));
+        Assert.assertFalse(map.containsKey(ReadableDateTime.class));
+        Assert.assertFalse(map.containsKey(ReadableInstant.class));
+        Assert.assertFalse(map.containsKey(Serializable.class));
+        Assert.assertTrue( map.containsKey(Instant.class));
+
+        map = new ClassToInstanceMultiMap<AbstractInstant>(true);
+        populate(map);
+        Assert.assertEquals(map.keys().size(), 9);
+        Assert.assertFalse(map.containsKey(null));
+        Assert.assertFalse(map.containsKey(Chronology.class));
+        Assert.assertTrue( map.containsKey(AbstractInstant.class));
+        Assert.assertTrue( map.containsKey(AbstractDateTime.class));
+        Assert.assertTrue( map.containsKey(BaseDateTime.class));
+        Assert.assertTrue( map.containsKey(DateTime.class));
+        Assert.assertTrue( map.containsKey(Comparable.class));
+        Assert.assertTrue( map.containsKey(ReadableDateTime.class));
+        Assert.assertTrue( map.containsKey(ReadableInstant.class));
+        Assert.assertTrue( map.containsKey(Serializable.class));
+        Assert.assertTrue( map.containsKey(Instant.class));
+    }
+
+    @Test public void testValuesAndContainsValues() {
+        ClassToInstanceMultiMap<AbstractInstant> map = new ClassToInstanceMultiMap<AbstractInstant>();
+
+        DateTime now = new DateTime();
+        map.put(now);
+
+        DateTime now100 = now.plus(100);
+        map.put(now100);
+
+        Instant instant = new Instant();
+        map.put(instant);
+
+        Assert.assertEquals(map.values().size(), 3);
+        Assert.assertFalse(map.containsValue(null));
+        Assert.assertFalse(map.containsValue(now.minus(100)));
+        Assert.assertFalse(map.containsValue(instant.minus(100)));
+        Assert.assertTrue( map.containsValue(instant));
+        Assert.assertTrue(map.containsValue(now));
+        Assert.assertTrue(map.containsValue(now100));
+    }
+
+    @Test public void testEquals() {
+        // TODO
+    }
+
+    @Test public void testGet() {
+        ClassToInstanceMultiMap<AbstractInstant> map = new ClassToInstanceMultiMap<AbstractInstant>();
+        populate(map);
+
+        List<?> values = map.get(null);
+        Assert.assertEquals(values.size(), 0);
+
+        values = map.get(DateTime.class);
+        Assert.assertEquals(values.size(), 2);
+
+        values = map.get(Instant.class);
+        Assert.assertEquals(values.size(), 1);
+    }
+
+    protected void populate(ClassToInstanceMultiMap<AbstractInstant> map) {
+        DateTime now = new DateTime();
+        map.put(now);
+
+        DateTime now100 = now.plus(100);
+        map.put(now100);
+
+        Instant instant = new Instant();
+        map.put(instant);
+    }
+}
